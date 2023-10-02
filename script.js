94a5c90202a99fc83ec8810639d9742d1ca3a76a
nepulkumar <124512972+nepulkumar@users.noreply.github.com>
2023-10-02 23:05:56 +0530

    Add files via upload
    final commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// double event
+
+
+const boxElement = document.getElementById("box");
+
+//adding the double click event listener to the element
+
+boxElement.addEventListener("dblclick", function(){
+    alert("double click event in Js working properly");
+})
